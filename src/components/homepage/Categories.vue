35ceaf32611650cd8172b1e8f16ea03c01a38695
raymondtju <raymond.rtju@gmail.com>
2022-10-23 19:26:31 +0700

    main

---
+++ src/components/homepage/Categories.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+import CategoriesCard from '../CategoriesCard.vue';
+
+const categories = ref([
+    { id: 1, title: "Mobile UI Kit", count: 78, image: "src/assets/img/categories-2.jpg" },
+    { id: 2, title: "Desktop UI Kit", count: 39210, image: "src/assets/img/categories-1.jpg" },
+    { id: 3, title: "Apple UI Kit", count: 234123, image: "src/assets/img/categories-3.jpg" },
+    { id: 4, title: "People UI Kit", count: 72348, image: "src/assets/img/categories-4.jpg" },
+])
+
+</script>
+<template>
+    <div class="container px-4 mx-auto my-16 md:px-12">
+        <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">Top Categories</h2>
+        <div class="flex flex-wrap -mx-1 lg:-mx-4">
+            <CategoriesCard v-for="category in categories" :key="category.id" :title="category.title"
+                :count="category.count" :image="category.image" />
+        </div>
+    </div>
+</template>
