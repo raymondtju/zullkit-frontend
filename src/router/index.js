2d46960f40a556a014b52bab2f3af5add6aff72c
Raymond Tju <raymond.rtju@gmail.com>
2022-10-24 15:26:05 +0700

    main

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import SignupView from '../views/SignupView.vue'
+import ProductView from '../views/ProductView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/signup',
       name: 'signup',
       component: SignupView
+    },
+    {
+      path: '/product',
+      name: 'product',
+      component: ProductView
     },
     // {
     //   path: '/about',
