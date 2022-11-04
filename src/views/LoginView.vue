8d4fc9c58bbe5d5e714fd7faeee5563a3a5c23b4
Raymond Tju <75233529+raymondtju@users.noreply.github.com>
2022-11-05 00:05:23 +0700

    main

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -8,7 +8,7 @@ import LoginForm from '../components/authentication/LoginForm.vue';
 			<div class="mx-auto max-w-7xl">
 				<div class="flex flex-col items-center w-full min-h-screen pt-6 sm:justify-center sm:pt-0">
 					<div class="w-full p-5 mx-auto sm:max-w-md">
-						<h2 class="mb-20 text-5xl font-bold text-center">Welcome Back</h2>
+						<h2 class="mb-20 text-5xl font-bold text-center">Welcome Back !</h2>
 						<LoginForm />
 					</div>
 					<div class="w-full mx-auto mt-20 sm:max-w-2xl">
